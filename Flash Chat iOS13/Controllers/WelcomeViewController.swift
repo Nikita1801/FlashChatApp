e8f69d6c979fe036fb194b3e478ed809fee1220b
nikitamakarevich <nikita-makarevich@mail.ru>
2022-02-16 23:24:13 +0300

    Made WelcomeScreen UI

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -5,11 +5,14 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var registrationButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        registrationButton.layer.cornerRadius = 30
+        logInButton.layer.cornerRadius = 30
     }
     
 
